e633422d4e2407681eaa895966b45165c04e2c36
jungwook123 <24sunrin119@sunrint.hs.kr>
2025-08-13 14:49:25 +0900

    playerGuarding

--- Assets/Scripts/HyunScript/Player.cs
+++ Assets/Scripts/HyunScript/Player.cs
@@ -6,10 +6,26 @@ using UnityEngine;
 public class Player : MonoBehaviourPunCallbacks, IHitable
 {
     
-    public float health = 100f;
+    private float health = 100f;
     public PlayerKickAttack kickAttack;
     public PlayerAttack punchAttack;
-    
+    private PlayerMove playerMove;
+
+    public float Health
+    {
+        get => health;
+
+        set
+        {
+            health = value;
+            // 체력 닳았을떄 이펙트 추가.
+        }
+    }
+
+    private void Start()
+    {
+        playerMove = GetComponent<PlayerMove>();
+    }
 
     void Update()
     {
@@ -29,6 +45,7 @@ public class Player : MonoBehaviourPunCallbacks, IHitable
 
     public void Hit(float damage)
     {
+        if (playerMove.guarding == true) return;
         health -= damage;
     }
 

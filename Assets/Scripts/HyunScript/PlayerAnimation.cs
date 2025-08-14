c41d30e0ca1236545f6ec1430b0f8f52cfb6beba
robedcloud23777 <robedcloud23777@gmail.com>
2025-08-15 01:46:12 +0900

    d

--- Assets/Scripts/HyunScript/PlayerAnimation.cs
+++ Assets/Scripts/HyunScript/PlayerAnimation.cs
@@ -4,6 +4,17 @@ public class PlayerAnimation : MonoBehaviour
 {
     public Animator animator;
 
+    [SerializeField] private int currentCombo = 0;
+    private float lastAttackTime;
+    public float comboResetTime = 1f;
+    private int maxCombo = 5;
+
+    private void Update()
+    {
+        if (Time.time - lastAttackTime > comboResetTime)
+            currentCombo = 0;
+    }
+
     public void SetMove(int horizontal, bool isGround, int verticalVelocity)
     {
         animator.SetInteger("MoveX", horizontal);
@@ -13,16 +24,53 @@ public class PlayerAnimation : MonoBehaviour
 
     public void TriggerJump()
     {
-        animator.SetTrigger("Jump");
+        if (!IsInState("Jump"))
+            animator.SetTrigger("Jump");
     }
 
     public void TriggerAttack()
     {
+        currentCombo++;
+        if (currentCombo > maxCombo) currentCombo = 1;
+        animator.SetInteger("ComboIndex", currentCombo);
         animator.SetTrigger("Attack");
+
+        lastAttackTime = Time.time;
     }
 
     public void TriggerKick()
     {
-        animator.SetTrigger("Kick");
+        if (!IsInState("Kick"))
+            animator.SetTrigger("Kick");
+    }
+
+    public bool IsInState(string stateName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    // 애니메이션 이벤트에서 호출 가능
+    public void ResetCombo()
+    {
+        currentCombo = 0;
+        animator.SetInteger("ComboIndex", 0);
+    }
+
+    public void TriggerHit()
+    {
+        if (!IsInState("Hit"))
+            animator.SetTrigger("Hit");
+    }
+
+    public void TriggerDodge()
+    {
+        if (!IsInState("Dodge"))
+            animator.SetTrigger("Dodge");
+    }
+
+    public void TriggerDie()
+    {
+        if (!IsInState("Die"))
+            animator.SetTrigger("Die");
     }
 }

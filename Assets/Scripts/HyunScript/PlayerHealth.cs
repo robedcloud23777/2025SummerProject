af9b6c2ce1f6b4d044f298db8d548e76230b1ab5
robedcloud23777 <robedcloud23777@gmail.com>
2025-08-28 00:44:58 +0900

    체력 닳음

--- Assets/Scripts/HyunScript/PlayerHealth.cs
+++ Assets/Scripts/HyunScript/PlayerHealth.cs
@@ -1,15 +1,54 @@
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviourPun, IDamageable
 {
-    public float hp = 100f;
+    public float health = 100f;
+    public float otherHealth = 100f;
+    private Slider HpSlider1;
+    private Slider HpSlider2;
+    private PlayerAnimation playerAnim;
 
     [PunRPC] public void RPC_TakeDamage(int dmg) => TakeDamage(dmg);
 
     public void TakeDamage(float damage)
     {
-        hp -= damage;
-        Debug.Log($"[HP] {name} -{damage} �� {hp}");
+        health -= damage;
+    }
+
+    void Start()
+    {
+        playerAnim = GetComponent<PlayerAnimation>();
+        HpSlider1 = GameObject.Find("Canvas/HPBAR1").GetComponent<Slider>();
+        HpSlider2 = GameObject.Find("Canvas/HPBAR2").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        HealthUI();
+
+        if (health < 0f)
+        {
+            playerAnim.TriggerDie();
+        }
+    }
+
+    public void HealthUI()
+    {
+        HpSlider1.value = health;
+        HpSlider2.value = otherHealth;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }
+        else
+        {
+            otherHealth = (float)stream.ReceiveNext();
+        }
     }
 }
